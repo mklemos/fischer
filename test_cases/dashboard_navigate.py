5b65c326fb663296165600b5dd63881ba8e911b7
Joshua M Alpert <jma858@humboldt.edu>
2017-11-09 14:03:51 -0800

    Added The start of the dashboard navigation module

---
+++ test_cases/dashboard_navigate.py
@@ -0,0 +1,13 @@
+import socket
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions
+
+def gotoRequestAccess(driver):
+    #Click on the requests tab
+    WebDriverWait(driver, 30).until(expected_conditions.presence_of_element_located((By.XPATH, "//a[contains(text(),'Requests')]"))).click()
+    #Click on the Request Access tab
+    WebDriverWait(driver, 30).until(expected_conditions.presence_of_element_located((By.XPATH, "//a[contains(text(),'Request Access')]"))).click()
