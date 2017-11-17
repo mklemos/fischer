03033e5214107ef1119c6bf9dc24adb5cde8595e
Joshua M Alpert <jma858@humboldt.edu>
2017-11-17 08:18:51 -0800

    Cleaned up official_websiteCheck

--- test_cases/official_website.py
+++ test_cases/official_website.py
@@ -17,25 +17,24 @@ non_employee_websiteDict = {"Create website":["Official website"], "Add a develo
 
 
 def official_websiteCheck(driver):
-        # dashboard_navigate.gotoRequestAccess(driver)
-        #
-        # dashboard_navigate.selectIncludeSelf(driver)
-        #
-        # try:
-        #     #Try to select offical website
-        #     dashboard_navigate.selectDropdownOption(driver, "Official website")
-        # except:
-        #     #We cant find offical website so something is broken
-        #     assert 2 == 1
+        try:
+            #Try to select offical website
+            dashboard_navigate.selectDropdownOption(driver, "Official website")
+        except:
+            #We cant find offical website so something is broken
+            assert 2 == 1
 
         for permision in offical_websiteDict:
 
             try:
                 #Try to select permison
                 dashboard_navigate.selectDropdownOption(driver, permision)
+                #Make sure each label exists on the page once we select the permision if the permison has labels
                 for label in offical_websiteDict[permision]:
+                    #Build xpath
                     xpathstart = "//label[contains(text(),'"
                     xpathend = "')]"
+                    #Run contains check using thestring of the label getting it from the dictionary
                     WebDriverWait(driver, 30).until(expected_conditions.presence_of_element_located((By.XPATH, xpathstart + label + xpathend)))
             except:
                 #We cant find permison in dropdown so fail the test
@@ -43,14 +42,24 @@ def official_websiteCheck(driver):
 
 def non_employee_websiteDict_Check(driver):
        # added this function so that non-employee types could have access to a sub-dictionary
+        try:
+            #Try to select offical website
+            dashboard_navigate.selectDropdownOption(driver, "Official website")
+        except:
+            #We cant find offical website so something is broken
+            assert 2 == 1
+
         for permision in non_employee_websiteDict:
 
             try:
                 #Try to select permison
                 dashboard_navigate.selectDropdownOption(driver, permision)
-                for label in non_employee_websiteDict[permision]:
+                #Make sure each label exists on the page once we select the permision if the permison has labels
+                for label in offical_websiteDict[permision]:
+                    #Build xpath
                     xpathstart = "//label[contains(text(),'"
                     xpathend = "')]"
+                    #Run contains check using thestring of the label getting it from the dictionary
                     WebDriverWait(driver, 30).until(expected_conditions.presence_of_element_located((By.XPATH, xpathstart + label + xpathend)))
             except:
                 #We cant find permison in dropdown so fail the test
@@ -67,6 +76,9 @@ class test_c_Test(unittest.TestCase):
         setup_login.fischer_login(self.driver, "sls1231")
 
     def test_c_Test(self):
+
+       dashboard_navigate.gotoRequestAccess(self.driver)
+       dashboard_navigate.selectIncludeSelf(self.driver)
        official_websiteCheck(self.driver)
 
 
