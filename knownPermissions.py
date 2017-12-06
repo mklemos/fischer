863bf8fb9d3e8138df41da3ad489a3bc61ec9635
Maximilian K Lemos <mkl177@humboldt.edu>
2017-12-06 15:09:28 -0800

    added excel processing csv to list, and created file that folds a dictionary of lists

---
+++ knownPermissions.py
@@ -0,0 +1,5 @@
+# knownPermissions.py
+
+
+# dictionary of lists
+{'sls1231':[['Network Folder', ''], ['Official website', 'Create website'], ['Official website', 'Add a developer'], ['Official website', 'Create MySQL accounts'], ['Official website', 'Reset website permissions'], ['Personal website', ''], ['Departmental Email Account', ''], ['Test', ''], ['IT Admin', ''], ['Search', '']]}
